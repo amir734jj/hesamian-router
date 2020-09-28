4406019fc9694d1735781507bd4e637b1f0d7c4d
Amir <hesamian@uwm.edu>
2020-09-27 22:01:13 -0500

    Improved the resume

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require "json"
 
 def resolve_url name
   "http://#{name}.hesamian.com"
@@ -12,14 +13,16 @@ get '/' do
         'name' => 'Hooman Hesamian',
         'email' => 'mailto:hooman@hesamian.com',
         'github' => 'https://github.com/hooman734/',
-        'linkedin' => 'https://www.linkedin.com/in/hesamyan/'
+        'linkedin' => 'https://www.linkedin.com/in/hesamyan/',
+        'resume' => 'https://docs.google.com/document/d/1WRdXjE4SV2XGeFJrxuVcg1RmHmaa01Vgey465b8jXPM/export?format=pdf'
       },
       {
         'url' => resolve_url('amir'),
         'name' => 'Amir Hesamian',
         'email' => 'mailto:amir@hesamian.com',
         'github' => 'https://github.com/amir734jj/',
-        'linkedin' => 'https://www.linkedin.com/in/hesamian/'
+        'linkedin' => 'https://www.linkedin.com/in/hesamian/',
+        'resume' => 'https://docs.google.com/document/d/1O99oMXhvu4vYd09WLvC9G2OWB3W3iNWJzYOVHBpVftI/export?format=pdf'
       }
     ]
   }
